3fff4ba1bd7e877ef510e454847f4b052260387f
Cody <williwonka14@gmail.com>
2017-12-22 11:49:00 -0800

    server

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 'use strict'
 
-const express = require('express')
-const cors = require('cors')
-const pg = require('pg')
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+const bodyParser = require('body-parser').urlencoded({extended: true});
 
-const app = express()
-const PORT = process.env.PORT
-const DATABASE_URL = process.env.DATABASE_URL
-const CLIENT_URL = process.env.CLIENT_URL
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
 
-const client = new pg.Client(DATABASE_URL)
-client.connect()
-client.on('error', console.error)
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
 
-app.use(cors())
+app.use(cors());
 
 app.get('/api/v1/books', (req, res) => {
-  client.query('SELECT * FROM books;')
-    .then(results => res.send(results.rows))
-    .catch(console.error)
-})
-
-app.get('/api/v1/books/:id', (req, res) => {
-  client.query(`SELECT * FROM books WHERE book_id=$1;`, [req.params.id])
-  // console.log('your getting books by id')
-    // .then(console.log(res))
-    .then(result => res.send(result.rows))
-    .catch(console.error);
+  client.query('select book_id, title, author, image_url from books;')
+  .then(results => res.send(results.rows))
+  .catch(console.error);
 });
 
-app.all('*', (req, res) => res.redirect(CLIENT_URL))
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.get('/api/v1/books/:id', (req, res) => {
+  client.query('select * from books where book_id=$1', [req.params.id])
+  .then(results => res.send(results.rows))
+  .catch(console.log);
+});
+
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  client.query(
+    'insert into books(title, author, isbn, image_url, description) values($1, $2, $3, $4, $5)',
+    [req.body.title, req.body.author, req.body.isbn, req.body.image_url, req.body.description]
+  )
+  .then(result => res.send('insertion complete'));
+});
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));

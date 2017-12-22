89d00e8392a97795ff58665fe1790195164eb8bf
sundownspecial <je.mendoza@live.com>
2017-12-22 10:59:09 -0800

    added post method

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ client.on('error', console.error)
 
 app.use(cors())
 
+bodyParser.urlencoded( {extended: false} )
+
 app.get('/api/v1/books', (req, res) => {
   client.query('SELECT * FROM books;')
     .then(results => res.send(results.rows))
@@ -24,11 +26,35 @@ app.get('/api/v1/books', (req, res) => {
 
 app.get('/api/v1/books/:id', (req, res) => {
   client.query(`SELECT * FROM books WHERE book_id=$1;`, [req.params.id])
-  // console.log('your getting books by id')
-    // .then(console.log(res))
     .then(result => res.send(result.rows))
     .catch(console.error);
 });
+
+app.post('/api/v1/books', (request, response) => {
+  // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
+  // PUT YOUR RESPONSE HERE
+  client.query(
+    `INSERT INTO
+    books (title, author, image_url, isbn, description)
+    VALUES ($1, $2, $3, $4, $5);
+    `,
+    [
+      request.body.title,
+      request.body.author,
+      request.body.image_url,
+      request.body.isbn,
+      request.body.description
+    ]
+  )
+    .then(function() {
+      response.send('insert complete')
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+});
+
+
 
 app.all('*', (req, res) => res.redirect(CLIENT_URL))
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
